148b78f49d792872a305aed9ebb0abe180c2f0c0
Zhang Zhuo <mycinbrin@gmail.com>
2022-07-07 12:36:23 +0800

    Update run.sh

--- halo2-snark-aggregator-solidity/waffle/run.sh
+++ halo2-snark-aggregator-solidity/waffle/run.sh
@@ -1,8 +1,8 @@
 nvm use v16
 npm run build
-export OUTPUT=../../../agg_play/output_20220706_094343_sushi
+export OUTPUT=outputs/output_20220706_094343_sushi
 npm run test
-export OUTPUT=../../../agg_play/output_20220706_154408_sushi
+export OUTPUT=outputs/output_20220706_154408_sushi
 npm run test
-export OUTPUT=../../../agg_play/output_20220707_010942_dao
+export OUTPUT=outputs/output_20220707_010942_dao
 npm run test
