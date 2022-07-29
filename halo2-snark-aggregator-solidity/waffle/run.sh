a97d2e36bd30d0eae3e131b2583459681c3be5b3
Zhang Zhuo <mycinbrin@gmail.com>
2022-07-29 21:23:49 +0800

    example

--- halo2-snark-aggregator-solidity/waffle/run.sh
+++ halo2-snark-aggregator-solidity/waffle/run.sh
@@ -1,8 +1,4 @@
-nvm use v16
+#nvm use v16
 npm run build
-export OUTPUT=outputs/output_20220706_094343_sushi
-npm run test
-export OUTPUT=outputs/output_20220706_154408_sushi
-npm run test
-export OUTPUT=outputs/output_20220707_010942_dao
+export OUTPUT=outputs/output_20220729_065458_nft
 npm run test
