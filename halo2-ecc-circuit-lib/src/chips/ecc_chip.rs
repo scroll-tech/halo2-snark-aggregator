a96b09313527c85efa105b58b868af00d1abbdb1
sinka <xgao@zoyoe.com>
2023-02-08 18:26:24 +1100

    conservative ecc opt

--- halo2-ecc-circuit-lib/src/chips/ecc_chip.rs
+++ halo2-ecc-circuit-lib/src/chips/ecc_chip.rs
@@ -67,7 +67,7 @@ impl<'a, C: CurveAffine, N: FieldExt> EccChip<'a, C, N> {
     }
 }
 
-const CONFIG_WINDOW_SIZE: usize = 4usize;
+const CONFIG_WINDOW_SIZE: usize = 3usize;
 
 pub trait EccChipOps<C: CurveAffine, N: FieldExt> {
     type AssignedScalar;
@@ -148,6 +148,10 @@ pub trait EccChipOps<C: CurveAffine, N: FieldExt> {
             .iter()
             .map(|s| self.decompose_scalar::<CONFIG_WINDOW_SIZE>(ctx, s))
             .collect::<Result<Vec<_>, _>>()?;
+        let windows_pair_in_be = windows_in_be
+            .chunks(2)
+            .collect::<Vec<_>>();
+
 
         let identity = self.assign_identity(ctx)?;
         let point_candidates: Vec<Vec<AssignedPoint<_, _>>> = points
@@ -186,9 +190,73 @@ pub trait EccChipOps<C: CurveAffine, N: FieldExt> {
             Ok(curr_candidates.first().unwrap().clone())
         };
 
+
+        /* for adjacent points, we pre calculate the sum of two points $c_a * p_a + c_b * p_b$ */
+        let point_sum_groups = point_candidates.chunks(2); // we group the adjacent points by pairs
+
+        let point_pair_candidate: Vec<Vec<AssignedPoint<_, _>>> = point_sum_groups
+            .into_iter()
+            .map(|pair| {
+                let mut cache = pair[0].clone();
+                for d in 1..(1 << CONFIG_WINDOW_SIZE) {
+                    for r in 0..(1<<CONFIG_WINDOW_SIZE) {
+                        if pair.len() == 2 {
+                            let mut ai = self.add(ctx, &mut cache[r], &pair[1][d])?;
+                            self.curvature(ctx, &mut ai)?;
+                            cache.push(ai)
+                        }
+                    }
+                };
+                Ok(cache)
+            })
+            .collect::<Result<Vec<_>, Error>>()?;
+
+
+        //println!("size of calculate cache:{:?}", *ctx.offset - start_offset);
+
+        let pick_candidate_of_pair = |ctx: &mut Context<N>,
+                              pi: usize,
+                              lbits_in_le: &[AssignedCondition<N>; CONFIG_WINDOW_SIZE],
+                              hbits_in_le: &[AssignedCondition<N>; CONFIG_WINDOW_SIZE]|
+         -> Result<AssignedPoint<C, N>, Error> {
+            let mut curr_candidates: Vec<AssignedPoint<_, _>> = point_pair_candidate[pi].clone();
+            for bit in lbits_in_le {
+                let mut next_candidates = vec![];
+                let len = curr_candidates.len() / 2;
+                let mut it = curr_candidates.iter_mut();
+
+                for _ in 0..len {
+                    let a0 = it.next().ok_or(Error::Synthesis)?;
+                    let a1 = it.next().ok_or(Error::Synthesis)?;
+
+                    let cell = self.bisec_point_with_curvature(ctx, &bit, a1, a0)?;
+                    next_candidates.push(cell);
+                }
+                curr_candidates = next_candidates;
+            }
+            for bit in hbits_in_le {
+                let mut next_candidates = vec![];
+                let len = curr_candidates.len() / 2;
+                let mut it = curr_candidates.iter_mut();
+
+                for _ in 0..len {
+                    let a0 = it.next().ok_or(Error::Synthesis)?;
+                    let a1 = it.next().ok_or(Error::Synthesis)?;
+
+                    let cell = self.bisec_point_with_curvature(ctx, &bit, a1, a0)?;
+                    next_candidates.push(cell);
+                }
+                curr_candidates = next_candidates;
+            }
+            Ok(curr_candidates.first().unwrap().clone())
+        };
+
+
         let mut acc: Option<AssignedPoint<C, N>> = None;
 
         let mut round_size = None;
+
+        let start_offset = *ctx.offset;
 
         for wi in 0..windows_in_be[0].len() {
             let mut get_inner =
@@ -206,11 +274,26 @@ pub trait EccChipOps<C: CurveAffine, N: FieldExt> {
                         _ => {
                             let c = *ctx.offset;
                             let mut inner_acc = None;
-                            for pi in 0..points.len() {
+                            for pi in 0..points.len()/2 {
+                                let mut ci = pick_candidate_of_pair(ctx, pi,
+                                  &windows_pair_in_be[pi][0][wi],
+                                  &windows_pair_in_be[pi][1][wi])?;
+                                match inner_acc {
+                                    None => inner_acc = Some(ci),
+                                    Some(_inner_acc) => {
+                                        //let p = self.add_unsafe(ctx, &mut ci, &_inner_acc)?;
+                                        let p = self.add(ctx, &mut ci, &_inner_acc)?;
+                                        inner_acc = Some(p);
+                                    }
+                                }
+                            }
+                            if points.len() % 2 == 1 {
+                                let pi = points.len() - 1;
                                 let mut ci = pick_candidate(ctx, pi, &windows_in_be[pi][wi])?;
                                 match inner_acc {
                                     None => inner_acc = Some(ci),
                                     Some(_inner_acc) => {
+                                        //let p = self.add_unsafe(ctx, &mut ci, &_inner_acc)?;
                                         let p = self.add(ctx, &mut ci, &_inner_acc)?;
                                         inner_acc = Some(p);
                                     }
@@ -232,13 +315,18 @@ pub trait EccChipOps<C: CurveAffine, N: FieldExt> {
                 None => acc = Some(inner_acc),
                 Some(mut _acc) => {
                     for _ in 0..CONFIG_WINDOW_SIZE {
+                        //_acc = self.double_unsafe(ctx, &mut _acc)?;
                         _acc = self.double(ctx, &mut _acc)?;
                     }
+                    //_acc = self.add_unsafe(ctx, &mut inner_acc, &_acc)?;
                     _acc = self.add(ctx, &mut inner_acc, &_acc)?;
                     acc = Some(_acc);
                 }
             }
         }
+
+        let total_offset = *ctx.offset - start_offset;
+        println!("roundsize is {:?}", total_offset);
 
         Ok(acc.unwrap())
     }
@@ -380,6 +468,40 @@ pub trait EccChipOps<C: CurveAffine, N: FieldExt> {
         };
         Ok(AssignedPoint::new(cx, cy, lambda.z))
     }
+
+    /* special addition which assumes that a and b are not equal and not zero */
+    fn add_unsafe (
+        &self,
+        ctx: &mut Context<N>,
+        a: &mut AssignedPoint<C, N>,
+        b: &AssignedPoint<C, N>,
+    ) -> Result<AssignedPoint<C, N>, Error> {
+        //let base_gate = self.base_gate();
+        let integer_chip = self.integer_chip();
+
+        //self.assert_neq(a.z, zero);
+        //self.assert_neq(b.z, zero);
+
+        let mut diff_x = integer_chip.sub(ctx, &a.x, &b.x)?;
+        let mut diff_y = integer_chip.sub(ctx, &a.y, &b.y)?;
+        let (x_eq, tangent) = integer_chip.div(ctx, &mut diff_y, &mut diff_x)?;
+
+        // If y is not equal and both points are neq identity then x are not equal
+        //self.assert_neq(diff_y, zero);
+
+        //let y_eq = integer_chip.is_zero(ctx, &mut diff_y)?;
+        //let eq = base_gate.and(ctx, &x_eq, &y_eq)?;
+
+        /* Not necessary to compute curvature for the case of a=b */
+        let tangent = AssignedCurvature::new(tangent, x_eq);
+        let mut lambda = tangent;
+
+        let p = self.lambda_to_point(ctx, &mut lambda, a, b)?;
+        let p = self.bisec_point(ctx, &a.z, b, &p)?;
+//      let p = self.bisec_point(ctx, &b.z, a, &p)?;
+        Ok(p)
+    }
+
     fn add(
         &self,
         ctx: &mut Context<N>,
@@ -406,6 +528,20 @@ pub trait EccChipOps<C: CurveAffine, N: FieldExt> {
 
         Ok(p)
     }
+
+    fn double_unsafe(
+        &self,
+        ctx: &mut Context<N>,
+        a: &mut AssignedPoint<C, N>,
+    ) -> Result<AssignedPoint<C, N>, Error> {
+        let base_gate = self.base_gate();
+        let curvature = self.curvature(ctx, a)?;
+        let mut p = self.lambda_to_point(ctx, &mut curvature.clone(), a, a)?;
+        //p.z = base_gate.bisec_cond(ctx, &a.z, &a.z, &p.z)?;
+        Ok(p)
+    }
+
+
     fn double(
         &self,
         ctx: &mut Context<N>,
